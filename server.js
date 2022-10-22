3b6d741e45cd49c4b6b42e01ec5c85a2a10fca99
KeithGalway <keith.galway@outlook.com>
2022-10-22 15:35:52 +0800

    Added pages
    Added Login and Register pages

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express')
+const res = require('express/lib/response')
 const app = express()
 
 app.set('view-engine', 'ejs')
 
 app.get('/', (req, res) => {
     res.render('index.ejs', { name: 'Isaac' })
+})
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs')
+})
+
+app.post('/reister', (req, res) => {
+    
 })
 
 app.listen(3000)
